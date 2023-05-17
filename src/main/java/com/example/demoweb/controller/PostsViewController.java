12f37c3b1c758f64b56bf7a0e3de0b401629ea05
Your Name <you@example.com>
2023-05-17 23:03:55 +0500

    init commit

---
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -0,0 +1,26 @@
+package com.example.demoweb.controller;
+
+import com.example.demoweb.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostsViewController {
+    @Autowired
+    PostService postsService;
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("appName", "Мои посты");
+        model.addAttribute("posts", postsService.listAllPosts());
+        return "list";
+    }
+
+    @ResponseBody
+    @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
+    public String single(@PathVariable("id") Long id) {
+        return "Здесь будет страница поста №" + id;
+    }
+}
---
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -0,0 +1,47 @@
+package com.example.demoweb.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Post implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String text;
+    private Date creationDate;
+    private int likes;
+
+    public Post() {
+    }
+
+    public Post(Long id, String text, Date creationDate) {
+        this.id = id;
+        this.text = text;
+        this.creationDate = creationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+}
